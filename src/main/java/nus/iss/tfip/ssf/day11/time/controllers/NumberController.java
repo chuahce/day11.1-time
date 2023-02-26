59f52aaf5c9cd4005f7e5e8868256e628a99b8e2
chuahce <choongeng.chuah@gmail.com>
2023-02-26 12:35:03 +0800

    SSF Assessment Revision day11-time

--- src/main/java/nus/iss/tfip/ssf/day11/time/controllers/NumberController.java
+++ src/main/java/nus/iss/tfip/ssf/day11/time/controllers/NumberController.java
@@ -1,10 +1,14 @@
 package nus.iss.tfip.ssf.day11.time.controllers;
 
-import java.util.security.SecureRandom;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
